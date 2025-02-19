cc3c510de5951dcf9076492e9fa21fd69bb91e7b
Marco Ames <marcoames5@gmail.com>
2025-02-19 14:54:45 -0300

    fix: Update API endpoint to use local development server

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,8 +10,8 @@ function App() {
   const handleSearch = async () => {
     try {
       // Send request to the Go backend
-      const response = await fetch(`https://animerecommendation-443313.rj.r.appspot.com/api/?anime=${animeTitle}`);
-      
+      //const response = await fetch(`https://animerecommendation-443313.rj.r.appspot.com/api/?anime=${animeTitle}`);
+      const response = await fetch(`http://localhost:8080/api/?anime=${animeTitle}`);
       // If the response is not OK, throw an error
       if (!response.ok) {
         throw new Error('Anime not found');
